93b9b4f3bee7b1a8958faefc4cf0442dae81dbf0
shenle <popwich@yahoo.com>
2015-02-24 22:26:14 -0500

    - wk7_proj_bkup_2

--- assignment/src/wk7_proj_d1/draw.java
+++ assignment/src/wk7_proj_d1/draw.java
@@ -30,6 +30,7 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 
 public class draw {
@@ -70,13 +71,10 @@ public class draw {
 	 */
 	private void initialize() {		
 		frm_drawing = new JFrame();
-		frm_drawing.setTitle("drawing");
+		frm_drawing.setTitle("Paint");
 		frm_drawing.setBounds(100, 100, 702, 535);
 		frm_drawing.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frm_drawing.getContentPane().setLayout(null);
 		final JLabel statusbar= new JLabel("default");
-		statusbar.setBounds(1, 4, 1, 3);
-		frm_drawing.getContentPane().add(statusbar);
 
 		    
 		Button lineButton = new Button("Line");
@@ -84,16 +82,12 @@ public class draw {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		lineButton.setBounds(10, 32, 85, 41);
-		frm_drawing.getContentPane().add(lineButton);
 		
 		Button ovalButton = new Button("Oval");
 		ovalButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		ovalButton.setBounds(101, 32, 85, 41);
-		frm_drawing.getContentPane().add(ovalButton);
 		
 		Button setColourRed_Button = new Button("RED");
 		setColourRed_Button.addActionListener(new ActionListener() {
@@ -102,8 +96,6 @@ public class draw {
 		});
 		setColourRed_Button.setBackground(Color.RED);
 		setColourRed_Button.setForeground(Color.BLACK);
-		setColourRed_Button.setBounds(400, 32, 53, 41);
-		frm_drawing.getContentPane().add(setColourRed_Button);
 		
 		Button setColourYellow_Button = new Button("Yellow");
 		setColourYellow_Button.addActionListener(new ActionListener() {
@@ -111,37 +103,109 @@ public class draw {
 			}
 		});
 		setColourYellow_Button.setBackground(Color.YELLOW);
-		setColourYellow_Button.setBounds(459, 32, 48, 41);
-		frm_drawing.getContentPane().add(setColourYellow_Button);
-		
-		Panel panel1 = new Panel();
-		panel1.setBounds(118, 145, 318, 245);
-		frm_drawing.getContentPane().add(panel1);
 		
 		canvas_1 = new canvasArea();
-		canvas_1.setForeground(Color.ORANGE);
-		panel1.add(canvas_1, BorderLayout.CENTER);
-		canvas_1.setBackground(Color.ORANGE);
-		canvas_1.setLayout(new FlowLayout(FlowLayout.CENTER, 250, 250));
-		
-		final JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(10, 461, 143, 36);
-		frm_drawing.getContentPane().add(lblNewLabel);
+		canvas_1.setForeground(Color.WHITE);
+		canvas_1.setBackground(Color.WHITE);
+		GroupLayout gl_canvas_1 = new GroupLayout(canvas_1);
+		gl_canvas_1.setHorizontalGroup(
+			gl_canvas_1.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 318, Short.MAX_VALUE)
+		);
+		gl_canvas_1.setVerticalGroup(
+			gl_canvas_1.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 245, Short.MAX_VALUE)
+		);
+		canvas_1.setLayout(gl_canvas_1);
+		GroupLayout groupLayout = new GroupLayout(frm_drawing.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(1)
+							.addComponent(statusbar, GroupLayout.PREFERRED_SIZE, 1, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(10)
+							.addComponent(lineButton, GroupLayout.PREFERRED_SIZE, 85, GroupLayout.PREFERRED_SIZE)
+							.addGap(6)
+							.addComponent(ovalButton, GroupLayout.PREFERRED_SIZE, 85, GroupLayout.PREFERRED_SIZE)
+							.addGap(214)
+							.addComponent(setColourRed_Button, GroupLayout.PREFERRED_SIZE, 53, GroupLayout.PREFERRED_SIZE)
+							.addGap(6)
+							.addComponent(setColourYellow_Button, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap())
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(canvas_1, GroupLayout.DEFAULT_SIZE, 676, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(4)
+					.addComponent(statusbar, GroupLayout.PREFERRED_SIZE, 3, GroupLayout.PREFERRED_SIZE)
+					.addGap(25)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(lineButton, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
+						.addComponent(ovalButton, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
+						.addComponent(setColourRed_Button, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
+						.addComponent(setColourYellow_Button, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE))
+					.addGap(36)
+					.addComponent(canvas_1, GroupLayout.PREFERRED_SIZE, 367, GroupLayout.PREFERRED_SIZE)
+					.addGap(0, 0, Short.MAX_VALUE))
+		);
+		frm_drawing.getContentPane().setLayout(groupLayout);
 		
 		JMenuBar menuBar = new JMenuBar();
 		frm_drawing.setJMenuBar(menuBar);
 		
-		JMenu mnNewMenu = new JMenu("New menu");
-		menuBar.add(mnNewMenu);
+		JMenu fileMenu = new JMenu("File");
+		menuBar.add(fileMenu);
 		
-		JMenuItem mntmNewMenuItem = new JMenuItem("New menu item");
-		mnNewMenu.add(mntmNewMenuItem);
+		JMenuItem mntmNew = new JMenuItem("New");
+		fileMenu.add(mntmNew);
 		
-		JMenu mnNewMenu_1 = new JMenu("New menu");
-		menuBar.add(mnNewMenu_1);
+		JMenuItem mntmOpen = new JMenuItem("Open");
+		fileMenu.add(mntmOpen);
 		
-		JMenuItem mntmNewMenuItem_1 = new JMenuItem("New menu item");
-		mnNewMenu_1.add(mntmNewMenuItem_1);
+		JMenuItem mntmSave = new JMenuItem("Save");
+		fileMenu.add(mntmSave);
+		
+		JMenuItem mntmSaveAs = new JMenuItem("Save as ...");
+		fileMenu.add(mntmSaveAs);
+		
+		JMenuItem mntmPrint = new JMenuItem("Print");
+		fileMenu.add(mntmPrint);
+		
+		JMenuItem exitMenuItem = new JMenuItem("Exit");
+		fileMenu.add(exitMenuItem);
+		
+		JMenu editMenu = new JMenu("Edit");
+		menuBar.add(editMenu);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Cut");
+		editMenu.add(mntmNewMenuItem_1);
+		
+		JMenuItem mntmPaste = new JMenuItem("Paste");
+		editMenu.add(mntmPaste);
+		
+		JMenuItem mntmDelete = new JMenuItem("Delete");
+		editMenu.add(mntmDelete);
+		
+		JMenuItem mntmSelectAll = new JMenuItem("Select all");
+		editMenu.add(mntmSelectAll);
+		
+		JMenu mnHelp = new JMenu("Help");
+		menuBar.add(mnHelp);
+		
+		JMenuItem mntmWelcome = new JMenuItem("Welcome");
+		mnHelp.add(mntmWelcome);
+		
+		JMenuItem mntmHelpContent = new JMenuItem("Help content");
+		mnHelp.add(mntmHelpContent);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("About Paint");
+		mnHelp.add(mntmNewMenuItem);
 		
 	}
 	public canvasArea getCanvas() {
