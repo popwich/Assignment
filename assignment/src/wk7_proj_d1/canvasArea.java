a9f32b9e51c907e55a46fe84eccf3d7f14159f88
shenle <popwich@yahoo.com>
2015-02-26 20:58:24 -0500

    - use switch case statment

--- assignment/src/wk7_proj_d1/canvasArea.java
+++ assignment/src/wk7_proj_d1/canvasArea.java
@@ -68,50 +68,52 @@ public class canvasArea extends JPanel {
 			
 	//constructor depends on what shapeSelected string is passed in from GUI
 	public void canvasArea_method(String shapeSelected) {
-		if (shapeSelected == "line") {
-			addMouseMotionListener(new MouseMotionAdapter() {
-				@Override
-				public void mouseDragged(MouseEvent e) {
-					currentLine.addPoint(e.getX(),  e.getY());
-					repaint();
-				}
-			});
-			
-			addMouseListener(new MouseAdapter() {
-				@Override
-				public void mousePressed(MouseEvent e) {
-				flag = "line";
-				currentLine = new PolyLine();
-				lines.add(currentLine);
-				currentLine.addPoint(e.getX(), e.getY());
-				}
-			});
+		switch (shapeSelected) {
+			case  "line":
+				addMouseMotionListener(new MouseMotionAdapter() {
+					@Override
+					public void mouseDragged(MouseEvent e) {
+						currentLine.addPoint(e.getX(),  e.getY());
+						repaint();
+					}
+				});
+				
+				addMouseListener(new MouseAdapter() {
+					@Override
+					public void mousePressed(MouseEvent e) {
+					flag = "line";
+					currentLine = new PolyLine();
+					lines.add(currentLine);
+					currentLine.addPoint(e.getX(), e.getY());
+					}
+				});
+				break;
+	
+			case "rect": 
+				addMouseMotionListener(new MouseMotionAdapter() {
+					@Override
+					public void mouseDragged(MouseEvent e) {
+						//currentLine.addPoint(e.getX(),  e.getY());
+						currentRect.width = Math.abs(e.getX() - currentRect.left);
+						currentRect.height = Math.abs(e.getY() - currentRect.top);
+						repaint();
+					}
+				});
+				
+				addMouseListener(new MouseAdapter() {
+					@Override
+					public void mousePressed(MouseEvent e) {
+					flag = "rect";
+					currentRect = new RectShape();
+					//currentLine = new PolyLine();
+					//lines.add(currentLine);
+					//currentLine.addPoint(e.getX(), e.getY());
+					currentRect.left = e.getX();
+					currentRect.top = e.getY();
+					}
+				});
+				break;
 		}
-		else {
-			if (shapeSelected == "rect") {
-			addMouseMotionListener(new MouseMotionAdapter() {
-				@Override
-				public void mouseDragged(MouseEvent e) {
-					//currentLine.addPoint(e.getX(),  e.getY());
-					currentRect.width = Math.abs(e.getX() - currentRect.left);
-					currentRect.height = Math.abs(e.getY() - currentRect.top);
-					repaint();
-				}
-			});
-			
-			addMouseListener(new MouseAdapter() {
-				@Override
-				public void mousePressed(MouseEvent e) {
-				flag = "rect";
-				currentRect = new RectShape();
-				//currentLine = new PolyLine();
-				//lines.add(currentLine);
-				//currentLine.addPoint(e.getX(), e.getY());
-				currentRect.left = e.getX();
-				currentRect.top = e.getY();
-				}
-			});
-		}}
 	}
 	
 	//swing component override paintComponenet method to provide custom behavior, callback by repaint()
@@ -121,14 +123,14 @@ public class canvasArea extends JPanel {
       g.setColor(Color.RED);
       
       switch (flag) {
-      case "rect":
-    	  currentRect.draw(g);      
-    	  break;
-      case "line":
-    	   for (PolyLine line: lines) {
-    		   line.draw(g);
-    	   }
-    	   break;   
+	      case "rect":
+	    	  currentRect.draw(g);      
+	    	  break;
+	      case "line":
+	    	   for (PolyLine line: lines) {
+	    		   line.draw(g);
+	    	   }
+	    	   break;   
       }
    }
 
